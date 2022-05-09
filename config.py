9aba83bbefad69d7aa2fa40fda4ca176a70e5285
Asror Mirzoev <coder2002a@gmail.com>
2022-05-09 17:44:05 +0300

    Changed config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-import os
-
-with open('.env', 'r') as environment_variables:
-    for line in environment_variables.readlines():
-        key, value = line.split(' = ')
-        os.environ[key] = value.strip()
+# import os
+#
+# with open('.env', 'r') as environment_variables:
+#     for line in environment_variables.readlines():
+#         key, value = line.split(' = ')
+#         os.environ[key] = value.strip()
 # BOT_TOKEN = os.environ["BOT_TOKEN"]
 # MUSIXMATCH_API_KEY = os.environ["MUSIXMATCH_API_KEY"]
 BOT_TOKEN = "5310805270:AAGgZ_v25a7jRfAzy0Di8AumP_UfG3v6ldg"
