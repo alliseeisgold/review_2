5dec018c13068224c1651e1970bd3d432b936fe4
Asror Mirzoev <coder2002a@gmail.com>
2022-05-09 16:44:27 +0300

    Added all needed files.

---
+++ main.py
@@ -0,0 +1,69 @@
+import telebot
+from config import *
+from musixmatch import Musixmatch
+from iso3166 import countries
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=["tracks"])
+def tracks_of_author(message):
+    author = " ".join(
+        [t.capitalize() for t in message.json["text"].split(" ")[1:]]
+    ).strip()
+    response, status = Musixmatch.get_tracks_of_author(author)
+    if status == 200:
+        ans = "Here's list of popular tracks of *" + author + "*:\n"
+        l = len(ans)
+        for track in response["message"]["body"]["track_list"]:
+            tr = track["track"]["track_name"]
+            album_name = track["track"]["album_name"]
+            url = track["track"]["track_share_url"]
+            ans += f"Track: [{tr}]({url})\n"
+            if album_name:
+                ans += "Album: *" + album_name + "*"
+            ans += "\n\n"
+        if l == len(ans):
+            ans += "Author " + author + " hasn't any tracks."
+        bot.send_message(message.chat.id, text=ans, parse_mode="Markdown")
+    else:
+        bot.send_message(message.chat.id, text=MUSIXMATCH_ERROR)
+
+
+@bot.message_handler(commands=["charts"])
+def get_charts_of_country(message):
+    country_code = " ".join(message.json["text"].split(" ")[1:]).strip()
+    if not country_code or len(country_code) == 0:
+        bot.send_message(
+            message.chat.id,
+            text=FORMAT_ISO3166_ERROR,
+        )
+        return
+    else:
+        response, status = Musixmatch.get_country_charts(country_code)
+    if status == 200:
+        try:
+            cc = countries.get(country_code.lower()).name
+        except:
+            bot.send_message(
+                message.chat.id,
+                text=FORMAT_ISO3166_ERROR,
+            )
+            return
+        answer = "Here's top chart of *" + cc + "*:\n"
+        for track in response["message"]["body"]["track_list"]:
+            tr = track["track"]["track_name"]
+            artist = track["track"]["artist_name"]
+            url = track["track"]["track_share_url"]
+            answer += f"Track: [{tr}]({url}) (lyrics)\nArtist: *" + artist \
+                      + "*\n\n"
+        bot.send_message(message.chat.id, text=answer, parse_mode="Markdown")
+    else:
+        bot.send_message(message.chat.id, text=MUSIXMATCH_ERROR)
+
+
+if __name__ == '__main__':
+    MUSIXMATCH_ERROR = """Couldn't connect to MusixMatch. Please try again.
+                           Anyway you can report it to me @alliseeisgoal. 
+                           Thanks! """
+    FORMAT_ISO3166_ERROR = "Please pass country code in the format of ISO3166. For example: /charts ru"
+    bot.polling(none_stop=True, timeout=5)
---
+++ musixmatch.py
@@ -0,0 +1,66 @@
+import requests
+import json
+from config import MUSIXMATCH_API_KEY
+
+URL = "https://api.musixmatch.com/ws/1.1/"
+
+
+class Musixmatch:
+
+    @staticmethod
+    def return_error(status_code: int):
+        return f"Error! Something went wrong.", status_code
+
+    @staticmethod
+    def get_tracks_of_author(author, page_size=5):
+        parameters = (
+                "track.search?q_artist="
+                + author
+                + "&page_size="
+                + str(page_size)
+                + "s_artist_rating=desc&s_track_rating=desc&quorum_factor=1"
+        )
+        req_url = URL + parameters + "&apikey=" + MUSIXMATCH_API_KEY
+        response = requests.get(
+            req_url,
+        )
+        if response.status_code == 200:
+            x = json.loads(response.content)
+            return x, response.status_code
+        Musixmatch.return_error(response.status_code)
+    @staticmethod
+    def get_country_charts(country_code="XW", page_size=5):
+        parameters = (
+                "chart.tracks.get?country="
+                + country_code
+                + "&page_size="
+                + str(page_size)
+                + "&apikey="
+                + MUSIXMATCH_API_KEY
+        )
+        req_url = URL + parameters
+        response = requests.get(req_url)
+        if response.status_code == 200:
+            x = json.loads(response.content)
+            return x, response.status_code
+        Musixmatch.return_error(response.status_code)
+
+    @staticmethod
+    def get_chart_artists(country_code="US", page=1, artists_num=5):
+        # This api provides you the list of the top artists of a given country.
+        parameters = (
+                "chart.artists.get?page="
+                + str(page)
+                + "&page_size="
+                + str(artists_num)
+                + "&country="
+                + country_code.lower()
+                + "&apikey="
+                + MUSIXMATCH_API_KEY
+        )
+        req_url = URL + parameters
+        response = requests.get(req_url)
+        if response.status_code == 200:
+            x = json.loads(response.content)
+            return x, response.status_code
+        Musixmatch.return_error(response.status_code)
