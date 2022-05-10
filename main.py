c2efa20808dabebfc4f75bf6dc355c53fd770c25
Asror Mirzoev <coder2002a@gmail.com>
2022-05-10 06:09:58 +0300

    Changed main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ lang = "en"
 
 @bot.message_handler(commands=["start"])
 def set_lang(message):
-    user = message.json['text']
+    user = message.json['from_user']
     bot.send_message(message.chat.id, text=user)
 
 
