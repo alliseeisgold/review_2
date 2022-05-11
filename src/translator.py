d5dfd9b997159d5a4b216e36aa43ba65b23d03f6
Asror Mirzoev <coder2002a@gmail.com>
2022-05-12 02:07:21 +0300

    Changed translator. Now uses google_translator module.

--- src/translator.py
+++ src/translator.py
@@ -1,56 +1,57 @@
-from src.google_trans_new import google_translator
+from googletrans import Translator
 
 
 class Translate:
 
     @staticmethod
-    def translation(text: str, to_lang: str):
-        if to_lang == 'en':
+    def translation(text: str, dest: str, src: str):
+        if src == dest:
             return text
-        translator = google_translator()
-        translate_text = translator.translate(text, lang_src='en', lang_tgt=to_lang)
-        return translate_text
+        translator = Translator()
+        translate_text = translator.translate(text, dest, src)
+        return translate_text.text
 
     @staticmethod
-    def translate_syntax(to_lang):
-        syntax = Translate.translation("syntax", to_lang).lower()
-        example = Translate.translation("for example", to_lang).lower()
-        description = Translate.translation("description", to_lang).lower()
-
+    def translate_syntax(dest: str, src='en'):
+        syntax = Translate.translation("syntax", dest, src).lower()
+        example = Translate.translation("for example", dest, src).lower()
+        description = Translate.translation("description", dest, src).lower()
         helper = {
             "/start or /restart": {
                 syntax: "/start",
                 example: "/start",
-                description: Translate.translation("Bot welcomes you and introduces itself.", to_lang)
+                description: Translate.translation("Bot welcomes you and introduces itself.",
+                                                   dest, src)
             },
             "/tracks": {
-                syntax: "/tracks " + Translate.translation(" singer_name", to_lang),
+                syntax: "/tracks " + Translate.translation(" singer_name", dest, src),
                 example: "/tracks justin bieber",
-                description: Translate.translation("Shows you top tracks of a given artist.", to_lang)
+                description: Translate.translation("Shows you top tracks of a given artist.",
+                                                   dest, src)
             },
             "/charts": {
-                syntax: "/charts " + Translate.translation(" country_code", to_lang),
+                syntax: "/charts " + Translate.translation(" country_code",
+                                                           dest, src),
                 example: "/charts ru",
-                description: Translate.translation("""Shows you top tracks of a given country.\
-            Statics of some countries are not displayed in it, so by default\
-            it shows the top tracks of the USA.""", to_lang)
+                description: Translate.translation(
+                    "Shows you top tracks of a given country. "
+                    "Statics of some countries are not displayed in it, "
+                    "so by default it shows the top tracks of the USA.",
+                    dest, src)
             },
             "/chart_artists": {
-                syntax: "/char_artists " + "_".join(
-                    Translate.translation("country code", to_lang)),
+                syntax: "/char_artists " + Translate.translation("country code",
+                                                                 dest, src),
                 example: "/char_artists ru",
-                description: Translate.translation("""The same as the description of /charts command.\
-                But instead of top tracks it shows most famous singers in the region.
-                """, to_lang)
+                description: Translate.translation(
+                    "The same as the description of /charts command. But instead of "
+                    "top tracks it shows most famous singers in the region.",
+                    dest, src)
             },
             "/lyrics": {
-                syntax: "/lyrics " + "_".join(Translate.translation("music name", to_lang)),
+                syntax: "/lyrics " + Translate.translation("music_name", dest, src),
                 example: "/lyrics genesis",
-                description: Translate.translation("Shows the music lyrics.", to_lang)
+                description: Translate.translation("Shows the music lyrics.", dest, src)
             }
         }
         return helper
-    @staticmethod
-    def common_words(to_lang):
-        album = Translate.translation("Album", to_lang)
-        author = Translate.translation("Author", to_lang)
