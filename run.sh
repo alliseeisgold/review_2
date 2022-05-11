eef43e13ec54352e2435bb9c3c48a688f3567a85
Asror Mirzoev <coder2002a@gmail.com>
2022-05-11 09:50:13 +0300

    Updated

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 
 export BOT_TOKEN=5310805270:AAGgZ_v25a7jRfAzy0Di8AumP_UfG3v6ldg
 export MUSIXMATCH_API_KEY=352f922227a47654a366d2edc58fc7ae
-pip install -r requiremenets.txt
+pip install -r requirements.txt
 python3 main.py
